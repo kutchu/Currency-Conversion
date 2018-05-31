b791376a3f3b2717463a6f1deacf26145c6ce86d
kuldeep0991 <kuldeep.srivastava@searshc.com>
2018-05-31 19:38:46 +0530

    Web Security

---
+++ Currency-Conversion-Backend/src/main/java/com/currency/conversion/security/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.currency.conversion.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by ksrivas on 5/30/2018.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_COOKIE = "access_token";
+    public static final String ACCOUNT_CLAIM = "accountIds";
+    public static final String REQUEST_ACCOUNT = "accountId";
+    public static final String PATH_ACCOUNT = "account";
+
+    @Autowired
+    private RESTAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint);
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/**/*.woff",
+                        "/**/*.woff2",
+                        "/**/*.ttf",
+                        "/webjars/**"
+                ).permitAll()
+                .antMatchers(HttpMethod.GET, "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/info").permitAll()
+                .antMatchers(HttpMethod.GET, "/health").permitAll()
+                .antMatchers("/rest/**").permitAll()
+                .antMatchers("/view/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+
+}
