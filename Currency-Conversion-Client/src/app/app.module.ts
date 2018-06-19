6560aa0c8353d4d5c6ce44408945c36a2acab569
kuldeep0991 <kuldeep.srivastava@searshc.com>
2018-06-19 11:17:59 +0530

    Adding for registration, login and user pages

--- Currency-Conversion-Client/src/app/app.module.ts
+++ Currency-Conversion-Client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
